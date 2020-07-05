6883d83770750c0aa06560387ed5bdd35d634b34
vishkid84 <vishalittr@gmail.com>
2020-07-05 12:05:33 +0000

    Connected Flask to MongoDB Atlas

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+from os import path  
+if path.exists("env.py"):      
+     import env 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'workspace_database'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
