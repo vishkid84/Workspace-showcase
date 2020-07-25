959d92c9a6effc8928ff16e89fd297bf009e8918
vishkid84 <vishalittr@gmail.com>
2020-07-25 16:15:59 +0000

    Added scroll to top feature for workspaces page

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,21 @@ function myFunction(sel) {
     if(sel.val()!='Balcony') this.form.submit();
   document.getElementById("demo").innerHTML = "You selected: " + x;
 }
+
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+});
